04e3c692abbe9dcde0929f09cbf45716152cde62
Matthias Molitor <matthias@molitor.solutions>
2018-11-27 09:59:34 +0100

    implemented Inverse Captcha sequence sum

--- src/2017/InverseCaptcha/InverseCaptcha.ts
+++ src/2017/InverseCaptcha/InverseCaptcha.ts
@@ -3,5 +3,15 @@
  * @see https://adventofcode.com/2017/day/1
  */
 export const sequenceSum = (sequence : String) => {
-    return 0;
+    if (sequence.length == 0) {
+        return 0;
+    }
+    const toCheck = sequence + sequence.charAt(0);
+    let sum = 0;
+    for (let i = 0; i < sequence.length; i++) {
+        if (toCheck.charAt(i) == toCheck.charAt(i + 1)) {
+            sum += parseInt(toCheck.charAt(i), 10);
+        }
+    }
+    return sum;
 };
