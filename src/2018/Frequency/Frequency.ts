0d2b8303b4cbd364d338a13fa5c866b5b846dc20
Matthias Molitor <matthias@molitor.solutions>
2018-12-01 17:55:27 +0100

    implemented detection of repeating frequencies

--- src/2018/Frequency/Frequency.ts
+++ src/2018/Frequency/Frequency.ts
@@ -7,8 +7,7 @@ export function frequency(oneFrequencyValuePerLine : string): number;
 export function frequency(frequencyValuesOrLines : number[]|string): number {
     let frequencyValues : number[];
     if (typeof frequencyValuesOrLines === "string") {
-        frequencyValues = frequencyValuesOrLines.split("\n")
-            .map(frquencyLine => parseInt(frquencyLine, 10))
+        frequencyValues = extractFrequencies(frequencyValuesOrLines)
     } else {
         frequencyValues = frequencyValuesOrLines
     }
@@ -16,6 +15,38 @@ export function frequency(frequencyValuesOrLines : number[]|string): number {
     return frequencyValues.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
 }
 
+export function firstRepeatedFrequency(frequencyValues : number[]): number;
+export function firstRepeatedFrequency(oneFrequencyValuePerLine : string): number;
 export function firstRepeatedFrequency(frequencyValuesOrLines : number[]|string): number {
-    return 0
+    let frequencyValues : number[];
+    if (typeof frequencyValuesOrLines === "string") {
+        frequencyValues = extractFrequencies(frequencyValuesOrLines)
+    } else {
+        frequencyValues = frequencyValuesOrLines
+    }
+    let currentFrequency = 0;
+    const seenValues : number[] = [currentFrequency];
+    for (let value of infinite(frequencyValues)) {
+        currentFrequency += value;
+        if (seenValues.indexOf(currentFrequency) !== -1) {
+            // We have seen this frequency twice.
+            return currentFrequency;
+        }
+        seenValues.push(currentFrequency);
+    }
+    throw new Error("Unexpected termination.");
+}
+
+function extractFrequencies(frequencyValuePerLine: string): number[] {
+    return frequencyValuePerLine.split("\n")
+        .map(frequencyLine => parseInt(frequencyLine, 10));
+}
+
+function* infinite(list : number[]):IterableIterator<number> {
+    const length = list.length;
+    let iteration = 0;
+    while (true) {
+        yield list[iteration % length];
+        iteration++
+    }
 }
