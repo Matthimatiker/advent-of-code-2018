4c135a85dd0a24f523e409e54ef40d02a7bff279
Matthias Molitor <matthias@molitor.solutions>
2018-12-04 19:44:33 +0100

    implemented event parsing

--- src/2018/04-GuardDuty/GuardDuty.ts
+++ src/2018/04-GuardDuty/GuardDuty.ts
@@ -8,8 +8,29 @@ export enum GuardEventType {
 
 export class GuardEvent {
 
+    /**
+     * Parses guard events from lines like the following:
+     *
+     *     [1518-11-01 00:00] Guard #10 begins shift
+     *     [1518-11-01 00:05] falls asleep
+     *     [1518-11-01 00:25] wakes up
+     *
+     * @param eventLine
+     */
     public static from(eventLine: string) : GuardEvent {
-        throw new Error("not implemented");
+        const date = eventLine.substr(1, 16);
+        if (eventLine.includes("begins shift")) {
+            const guardId = eventLine.substring(18)
+                .replace("Guard #", "")
+                .replace(" begins shift", "");
+            return new GuardEvent(date, parseInt(guardId, 10), GuardEventType.BEGIN_SHIFT);
+        } else if (eventLine.includes("falls asleep")) {
+            return new GuardEvent(date, null, GuardEventType.FALL_ASLEEP);
+        } else if (eventLine.includes("wakes up")) {
+            return new GuardEvent(date, null, GuardEventType.WAKE_UP);
+        } else {
+            throw new Error(`Cannot parse event from line: ${eventLine}`)
+        }
     }
 
     public constructor(
@@ -17,6 +38,9 @@ export class GuardEvent {
         public readonly guard: Guard|null,
         public readonly type: GuardEventType
     ) {
+        if (type === GuardEventType.BEGIN_SHIFT && guard === null) {
+            throw new Error('Guard ID must be provided for BEGIN_SHIFT events.');
+        }
     }
 }
 
