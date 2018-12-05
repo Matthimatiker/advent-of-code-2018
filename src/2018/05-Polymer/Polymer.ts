bfa7d59c07a94bff150201a749d3c7897577c914
Matthias Molitor <matthias@molitor.solutions>
2018-12-05 19:31:37 +0100

    implemented react() function

--- src/2018/05-Polymer/Polymer.ts
+++ src/2018/05-Polymer/Polymer.ts
@@ -1,6 +1,25 @@
 declare type Polymer = string;
 
+const lowerCaseCharacters : string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const reactingUnits = [];
+for (let character of lowerCaseCharacters) {
+    reactingUnits.push(character + character.toUpperCase(), character.toUpperCase() + character)
+}
+const reactionRegExp = new RegExp(`(${reactingUnits.join('|')})`);
 
+/**
+ * Removes units in the polymer that can react (same type, different polarity).
+ *
+ * @param polymer
+ * @see https://adventofcode.com/2018/day/5
+ */
 export function react(polymer: Polymer): Polymer {
-    return "";
+    let reactedPolymer = polymer;
+    let removedUnits: number = 0;
+    do {
+        const previousUnits = reactedPolymer.length;
+        reactedPolymer = reactedPolymer.replace(reactionRegExp, '');
+        removedUnits = previousUnits - reactedPolymer.length;
+    } while(removedUnits > 0);
+    return reactedPolymer
 }
