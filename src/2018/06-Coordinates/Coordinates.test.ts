95ae0b1b1c2a2d3ced8dbd3e9fe52e255609855e
Matthias Molitor <matthias@molitor.solutions>
2018-12-07 00:44:51 +0100

    implemented COordinate tests

---
+++ src/2018/06-Coordinates/Coordinates.test.ts
@@ -0,0 +1,61 @@
+import {Coordinate} from "./Coordinates";
+import {expect} from "chai";
+
+describe('Coordinate', () => {
+    it('provides correct coordinates', () => {
+        const coordinate = new Coordinate(3, 5);
+
+        expect(coordinate.x).to.equal(3);
+        expect(coordinate.y).to.equal(5);
+    });
+
+    describe('#manhattanDistance()', () => {
+        it('returns zero for same coordinate', () => {
+            const coordinate = new Coordinate(3, 5);
+
+            const distance = coordinate.manhattanDistance(new Coordinate(3, 5));
+
+            expect(distance).to.equal(0);
+        });
+
+        it('returns correct value for coordinates that differ on x axis', () => {
+            const coordinate = new Coordinate(3, 5);
+
+            const distance = coordinate.manhattanDistance(new Coordinate(5, 5));
+
+            expect(distance).to.equal(2);
+        });
+
+        it('returns correct value for coordinates that differ on y axis', () => {
+            const coordinate = new Coordinate(3, 5);
+
+            const distance = coordinate.manhattanDistance(new Coordinate(3, 7));
+
+            expect(distance).to.equal(2);
+        });
+
+        it('returns correct value for coordinates that differ on both axis', () => {
+            const coordinate = new Coordinate(3, 5);
+
+            const distance = coordinate.manhattanDistance(new Coordinate(4, 7));
+
+            expect(distance).to.equal(3);
+        });
+
+        it('returns correct values if coordinates are negative', () => {
+            const coordinate = new Coordinate(-3, -5);
+
+            const distance = coordinate.manhattanDistance(new Coordinate(-4, -7));
+
+            expect(distance).to.equal(3);
+        });
+
+        it('returns correct values if negative coordinate is compared to positive coordinate', () => {
+            const coordinate = new Coordinate(-2, -3);
+
+            const distance = coordinate.manhattanDistance(new Coordinate(2, 3));
+
+            expect(distance).to.equal(10);
+        });
+    });
+});
--- src/2018/06-Coordinates/Coordinates.ts
+++ src/2018/06-Coordinates/Coordinates.ts
@@ -6,3 +6,4 @@ export class Coordinate {
         return -1;
     }
 }
+
