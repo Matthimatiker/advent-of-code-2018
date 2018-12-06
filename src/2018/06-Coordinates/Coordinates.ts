9089ed63ab65e0ce06847ef306f4f4ca094911a4
Matthias Molitor <matthias@molitor.solutions>
2018-12-06 20:20:29 +0100

    prepared stub for Coordinate class

---
+++ src/2018/06-Coordinates/Coordinates.ts
@@ -0,0 +1,8 @@
+export class Coordinate {
+    constructor(public readonly x: number, public readonly y: number) {
+    }
+
+    public manhattanDistance(anotherCoordinate: Coordinate): number {
+        return -1;
+    }
+}
