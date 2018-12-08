0f11e1872e4105e9b89298c73ecc1f582978bac9
Matthias Molitor <matthias@molitor.solutions>
2018-12-08 21:04:17 +0100

    implemented tests

--- src/2018/06-Coordinates/Coordinates.test.ts
+++ src/2018/06-Coordinates/Coordinates.test.ts
@@ -1,4 +1,4 @@
-import {Coordinate} from "./Coordinates";
+import {BoundedGrid, Coordinate} from "./Coordinates";
 import {expect} from "chai";
 
 describe('Coordinate', () => {
@@ -78,37 +78,70 @@ describe('Coordinate', () => {
 describe('BoundedGrid', () => {
     describe('constructor()', () => {
         it('rejects coordinates if topLeft and bottomRight are swapped', () => {
+            const topLeft = new Coordinate(0, 1);
+            const bottomRight = new Coordinate(5, 7);
 
+            expect(() => {
+                new BoundedGrid(bottomRight, topLeft);
+            }).to.throw(Error);
         });
     });
 
     describe('#isOnEdge()', () => {
-       it('returns true if given coordinate is top left', () => {
+        const topLeft = new Coordinate(0, 1);
+        const bottomRight = new Coordinate(5, 7);
+        const grid = new BoundedGrid(topLeft, bottomRight);
 
-       }) ;
+        it('returns true if given coordinate is top left', () => {
+            expect(grid.isOnEdge(topLeft)).to.equal(true);
+        });
 
         it('returns true if given coordinate is in the middle of the right edge', () => {
+            const coordinate = new Coordinate(3, 7);
 
-        }) ;
+            expect(grid.isOnEdge(coordinate)).to.equal(true);
+        });
 
         it('returns false if given coordinate is within grid', () => {
+            const coordinate = new Coordinate(3, 3);
 
-        }) ;
+            expect(grid.isOnEdge(coordinate)).to.equal(false);
+        });
     });
 
     describe('#getCoordinates()', () => {
         it('returns all coordinates in the grid', () => {
+            const topLeft = new Coordinate(0, 1);
+            const bottomRight = new Coordinate(3, 2);
+            const grid = new BoundedGrid(topLeft, bottomRight);
 
+            const coordinates = grid.getCoordinates();
+
+            expect(coordinates).to.have.lengthOf(4 * 2);
         });
     });
 
     describe('#createEnclosing()', () => {
         it('throws exception if given coordinate list is empty', () => {
-
+            expect(() => {
+                BoundedGrid.createEnclosing([]);
+            }).to.throw(Error);
         });
 
         it('returns grid that spans all given coordinates', () => {
+            const coordinates = [
+                // Bottom left
+                new Coordinate(3, 1),
+                // Within grid
+                new Coordinate(2, 2),
+                // Top right
+                new Coordinate(1, 4)
+            ];
 
+            const grid = BoundedGrid.createEnclosing(coordinates);
+
+            expect(grid.topLeft).to.deep.equal(new Coordinate(1, 1));
+            expect(grid.bottomRight).to.deep.equal(new Coordinate(3, 4));
         });
     });
 });
