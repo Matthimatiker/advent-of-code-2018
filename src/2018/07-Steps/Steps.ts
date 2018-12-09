84c07572bf43366d7e699b4af0697f7edbed63cb
Matthias Molitor <matthias@molitor.solutions>
2018-12-09 13:09:29 +0100

    implemented Rule and Step

--- src/2018/07-Steps/Steps.ts
+++ src/2018/07-Steps/Steps.ts
@@ -1,8 +1,17 @@
 type StepName = string
 
 export class Rule {
+
+    /**
+     * Extracts a rule from a line like the following:
+     *
+     *     'Step N must be finished before step T can begin.'
+     *
+     * @param ruleLine
+     */
     public static from(ruleLine: string): Rule {
-        throw new Error('Not implemented');
+        const words = ruleLine.split(' ');
+        return new Rule(words[7], words[1]);
     }
 
     public constructor(public readonly step: StepName, public readonly required: StepName) {
@@ -11,7 +20,19 @@ export class Rule {
 
 export class Step {
     public static fromRules(rules: Rule[]): Step[] {
-        throw new Error('Not implemented');
+        const allStepNames = new Set<StepName>();
+        rules.forEach((rule) => {
+            allStepNames.add(rule.step);
+            allStepNames.add(rule.required);
+        });
+        const steps: Step[] = [];
+        allStepNames.forEach((stepName) => {
+            const requirements = rules
+                .filter((rule) => rule.step === stepName)
+                .map((rule) => rule.required);
+            steps.push(new Step(stepName, requirements));
+        });
+        return steps;
     }
 
     public constructor(public readonly name: StepName, public readonly requirements: StepName[]) {
@@ -19,7 +40,7 @@ export class Step {
     }
 
     public requirementsFulfilledBy(alreadyCompleted: StepName[]) {
-        return false;
+        return this.requirements.every((requirement) => alreadyCompleted.includes(requirement));
     }
 }
 
