0f9f9e8ac6bd04650e6c93b67ee8564ee4960697
Matthias Molitor <matthias@molitor.solutions>
2018-12-09 05:43:11 +0100

    improved model for puzzle

--- src/2018/07-Steps/Steps.ts
+++ src/2018/07-Steps/Steps.ts
@@ -1,9 +1,24 @@
+type StepName = string
+
+export class Rule {
+    public static from(ruleLine: string): Rule {
+        throw new Error('Not implemented');
+    }
+
+    public constructor(public readonly step: StepName, public readonly required: StepName) {
+    }
+}
+
 export class Step {
-    constructor(public readonly name: string, public readonly requirements: Step[]) {
+    public static fromRules(rules: Rule[]): Step[] {
+        throw new Error('Not implemented');
+    }
+
+    public constructor(public readonly name: StepName, public readonly requirements: StepName[]) {
 
     }
 
-    public isReady(alreadyCompleted: Step[]) {
+    public requirementsFulfilledBy(alreadyCompleted: StepName[]) {
         return false;
     }
 }
