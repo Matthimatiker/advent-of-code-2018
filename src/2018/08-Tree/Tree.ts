2e37c0aa3643aac4019f9a113692f26343e48c87
Matthias Molitor <matthias@molitor.solutions>
2018-12-11 21:27:47 +0100

    implemented Tree parser

--- src/2018/08-Tree/Tree.ts
+++ src/2018/08-Tree/Tree.ts
@@ -7,8 +7,34 @@ export class Node {
     }
 }
 
+interface NodeParseResult {
+    node: Node,
+    newPosition: number
+}
+
 export class TreeParser {
     public static parse(data: number[]): Node {
-        throw new Error('Not implemented')
+        return this.parseNode(data, 0).node;
+    }
+
+    private static parseNode(data: number[], currentPosition: number): NodeParseResult {
+        const numberOfChildNodes = data[currentPosition];
+        const numberOfMetadataEntries = data[currentPosition + 1];
+        let newPosition = currentPosition + 2;
+        const childNodes = [];
+        for (let i = 0; i < numberOfChildNodes; i++) {
+            const result = this.parseNode(data, newPosition);
+            childNodes.push(result.node);
+            newPosition = result.newPosition;
+        }
+        const metadata = [];
+        for (let i = 0; i < numberOfMetadataEntries; i++) {
+            metadata.push(data[newPosition]);
+            newPosition++;
+        }
+        return {
+            node: new Node(childNodes, metadata),
+            newPosition: newPosition
+        };
     }
 }
