2afe1ee7e7ba61ebefcfed7f3596cd03c748b450
Matthias Molitor <matthias@molitor.solutions>
2018-12-11 21:52:22 +0100

    solved puzzle part 1

--- src/2018/08-Tree/puzzle.ts
+++ src/2018/08-Tree/puzzle.ts
@@ -1,3 +1,8 @@
 import puzzleInput from './puzzle-input.txt'
+import {TreeParser} from "./Tree";
 
-console.log(puzzleInput);
+const data = puzzleInput.split(' ').map(value => parseInt(value, 10));
+
+const root = TreeParser.parse(data);
+
+console.log(`Metadata sum: ${root.metadataSum()}`);
