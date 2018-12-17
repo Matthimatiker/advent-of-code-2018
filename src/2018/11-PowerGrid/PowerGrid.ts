635bc62905c7b633180ef769a81bceedb0ab9fa1
Matthias Molitor <matthias@molitor.solutions>
2018-12-18 00:12:50 +0100

    implemented Grid

--- src/2018/11-PowerGrid/PowerGrid.ts
+++ src/2018/11-PowerGrid/PowerGrid.ts
@@ -10,21 +10,42 @@ export class CellPosition {
 export class Grid {
 
     public constructor(private gridSerialNumber: number, public readonly topLeft: CellPosition, private bottomRight: CellPosition) {
-
     }
 
     public getPowerLevelAt(position: CellPosition): number {
-        throw new Error("not implemented");
+        const rackId = position.x + 10;
+        let powerLevel = rackId * position.y;
+        powerLevel += this.gridSerialNumber;
+        powerLevel *= rackId;
+        const powerLevelAsString = powerLevel.toString(10);
+        if (powerLevelAsString.length < 3) {
+            return 0;
+        }
+        const hundredsDigit = parseInt(powerLevelAsString.charAt(powerLevelAsString.length - 3), 10);
+        return hundredsDigit - 5;
     }
 
     public getTotalPower(): number {
-        throw new Error("not implemented");
+        let power = 0;
+        for (let x = this.topLeft.x; x <= this.bottomRight.x; x++) {
+            for (let y = this.topLeft.y; y <= this.bottomRight.y; y++) {
+                power += this.getPowerLevelAt(new CellPosition(x, y));
+            }
+        }
+        return power;
     }
 
     /**
      * Returns the 3x3 square with largest power in this grid.
      */
     public getSquareWithLargestTotalPower(): Grid {
-        throw new Error("not implemented")
+        const possibleSquares = [];
+        for (let x = this.topLeft.x; x <= this.bottomRight.x - 2; x++) {
+            for (let y = this.topLeft.y; y <= this.bottomRight.y - 2; y++) {
+                const square = new Grid(this.gridSerialNumber, new CellPosition(x, y), new CellPosition(x + 2, y + 2));
+                possibleSquares.push(square);
+            }
+        }
+        return possibleSquares.sort((left, right) => -1 * (left.getTotalPower() - right.getTotalPower()))[0];
     }
 }
